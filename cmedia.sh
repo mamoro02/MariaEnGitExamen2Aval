d01625db97d79ba265d5f0d2c6329e6015b4dadf
mamoro02 <mamoro02@floridauniversitaria.es>
2023-02-16 09:27:38 +0100

    cmedia

---
+++ cmedia.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+lineas=`cat ./consumo.txt | wc -l`
+
+sum=0
+j=0
+for i in `seq 2 $lineas`
+do
+ciudad=`cat ./consumo.txt | head -n$i | tail -n1 | awk '{print $1}'`
+if [ $1 == $ciudad ];
+    then
+        consumo=`cat ./consumo.txt | head -n$i | tail -n1 | awk '{print $4}'`
+        sum=$((sum+consumo))
+        j=$((j+1))
+    fi
+done
+
+resultado=$((sum/j))
+echo "La media de $1 es $resultado"
